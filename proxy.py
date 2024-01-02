dfd6d04d17ec25f5844c5c22a02779c5ec6d87d6
Espresso0org <95757209+Espresso0org@users.noreply.github.com>
2024-01-03 00:54:06 +0100

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -14,30 +14,24 @@ for proxy in proxy_list:
         chrome_options = Options()
         chrome_options.add_argument(f"--proxy-server=http://{proxy}")
 
-        for website_url in urls:
-            print(f"Opening URL: {website_url}")
-            driver = webdriver.Chrome(executable_path=path_to_webdriver, options=chrome_options)
-            driver.get(website_url)
+        for _ in range(5):
+            for website_url in urls:
+                print(f"Opening URL: {website_url}")
+                driver = webdriver.Chrome(executable_path=path_to_webdriver, options=chrome_options)
+                driver.get(website_url)
 
-            error_element = driver.find_elements(By.CSS_SELECTOR, 'div.icon.icon-generic')
+                error_element = driver.find_elements(By.CSS_SELECTOR, 'div.icon.icon-generic')
 
-            if len(error_element) > 0: 
-                print("Proxy is bad")
-                driver.quit()
-                continue
-            else:
-                print("Good proxy")
-                time.sleep(5)
-                # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-                for _ in range(5):
-                    print(f"Opening URL: {website_url} (Attempt {_ + 1} of 5)")
-                    time.sleep(5)
+                if len(error_element) > 0: 
+                    print("Proxy is bad")
                     driver.quit()
-                    driver = webdriver.Chrome(executable_path=path_to_webdriver, options=chrome_options)
-                    driver.get(website_url)
-                
-            driver.quit()
+                    continue
+                else:
+                    print("Good proxy")
+                    time.sleep(5)
+                    # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+                driver.quit()
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
