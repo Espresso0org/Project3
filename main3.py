2fe226d75cbd8bed272daddfbbedb1ee1326fcdd
Espresso0org <95757209+Espresso0org@users.noreply.github.com>
2024-02-16 03:07:10 +0100

    Update main3.py

--- main3.py
+++ main3.py
@@ -1,39 +1,6 @@
 python
-path_to_webdriver = 'chromedriver'
-
-proxy_list = []
-with open("proxy.txt", "r", encoding="utf-8") as file:
-    proxy_list = file.read().strip().split("\n")
-
-with open("url.txt", "r", encoding="utf-8") as file:
-    urls = file.read().strip().split("\n")
-
-for website_url in urls:
-    print(f"Opening URL: {website_url}")
-    for proxy in proxy_list:
-        print(f"Trying proxy: {proxy}")
-        try:
-            chrome_options = Options()
-            chrome_options.add_argument(f"--proxy-server=http://{proxy}")
-
-            driver = webdriver.Chrome(executable_path=path_to_webdriver, options=chrome_options)
-            driver.get(website_url)
-
-            error_element = driver.find_element(By.CSS_SELECTOR, 'div.icon.icon-generic')
-
-            print("Good proxy")
-            time.sleep(5)
-            # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        except NoSuchElementException:
-            print("Proxy is bad")
-            driver.quit()
-            continue
-
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
-            driver.quit()
-            continue
-
-        driver.quit()
-        break  # Break out of the proxy loop if a successful proxy is found
+def update_points(row_num, points, column_index, column_name):
+    current_points = int(values[row_num][column_index]) if values[row_num][column_index] else 0
+    new_points = current_points + points
+    worksheet.update_cell(row_num + 1, column_index + 1, str(new_points))
+    print(f"{column_name} points updated successfully to {new_points}!")
