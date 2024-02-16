046c0a70c06bf0dc4d4a94a4d5d89a6ea7181e71
Espresso0org <95757209+Espresso0org@users.noreply.github.com>
2024-02-16 19:41:41 +0100

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -9,12 +9,17 @@ selected_rows = []
 def print_non_empty_values(values, name_family_index):
     count = 1
     ranks_to_color = ["Deputy I", "Deputy II", "Deputy III", "Senior Deputy", "Corporal", "Sergeant", "Lieutenant"]
-    
+    colored_ranks = []
+
     for i, row in enumerate(values[1:], start=1):  # Skip the header row
         if row[name_family_index] != '':
             if row[name_family_index] in ranks_to_color:
-                print(f"{count}. {Fore.MAGENTA}{row[name_family_index]}{Style.RESET_ALL}")
+                colored_ranks.append(row[name_family_index])
             else:
                 print(f"{count}. {row[name_family_index]}")
-            selected_rows.append(i)
-            count += 1
+                selected_rows.append(i)
+                count += 1
+
+    # Display colored ranks separately with auto-assigned numbers
+    for i, rank in enumerate(colored_ranks, start=1):
+        print(f"{i}. {Fore.MAGENTA}{rank}{Style.RESET_ALL}")
