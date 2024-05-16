1337a3493f27c6faacacabe8c7684e4a2b758ad1
Espresso0org <95757209+Espresso0org@users.noreply.github.com>
2024-05-16 16:50:44 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 import pickle
-import base64
+import time
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+import base64
+import email
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
 
 def authenticate():
     creds = None
@@ -26,17 +28,31 @@ def authenticate():
     service = build('gmail', 'v1', credentials=creds)
     return service
 
-def check_new_emails():
+def mark_as_read(message_id):
     service = authenticate()
-    results = service.users().messages().list(userId='me', labelIds=['INBOX']).execute()
+    service.users().messages().modify(userId='me', id=message_id, body={'removeLabelIds': ['UNREAD']}).execute()
+
+def get_email_content(message_id):
+    service = authenticate()
+    message = service.users().messages().get(userId='me', id=message_id, format='full').execute()
+    msg_str = base64.urlsafe_b64decode(message['payload']['parts'][0]['body']['data']).decode('utf-8')
+    msg = email.message_from_string(msg_str)
+    print('From:', msg['From'])
+    print('Subject:', msg['Subject'])
+    print('Message:', msg.get_payload())
+
+def check_and_process_unread_emails():
+    service = authenticate()
+    results = service.users().messages().list(userId='me', labelIds=['INBOX', 'UNREAD']).execute()
     messages = results.get('messages', [])
 
-    if not messages:
-        print('No new messages.')
-    else:
+    if messages:
         for message in messages:
-            msg = service.users().messages().get(userId='me', id=message['id']).execute()
-            print('New Message Subject:', msg['payload']['headers'][17]['value'])
+            message_id = message['id']
+            get_email_content(message_id)
+            mark_as_read(message_id)
 
 if __name__ == '__main__':
-    check_new_emails()
+    while True:
+        check_and_process_unread_emails()
+        time.sleep(5)
